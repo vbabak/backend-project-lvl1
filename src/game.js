3a2595c1ca1c926cab5fbc26ca42547c9847dca0
Vladyslav Babak <marmalade.vlad@gmail.com>
2020-10-06 09:07:30 +0300

    cognitive complexity refactoring

--- src/game.js
+++ src/game.js
@@ -5,33 +5,77 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min)) + Math.ceil(min));
 }
 
+function printWelcome() {
+  console.log('Welcome to the Brain Games!');
+}
+
+function printGreeting(name) {
+  console.log(`Hello, ${name}`);
+}
+
+function printGameResults(won, name) {
+  if (won) {
+    console.log(`Congratulations, ${name}!`);
+  } else {
+    console.log(`Let's try again, ${name}!`);
+  }
+}
+
+function printInstructions() {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+}
+
+function printIterateAnswerRight() {
+  console.log('Correct!');
+}
+
+function printIterateAnswerWrong(numAnswer, correctAnswer) {
+  console.log(`'${numAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+}
+
 async function askQuestion(question) {
   const answer = await promptly.prompt(`${question}`);
   return answer;
 }
 
-const game = async () => {
-  console.log('Welcome to the Brain Games!');
+async function getName() {
   const name = await askQuestion('May I have your name?');
-  console.log(`Hello, ${name}`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  return name;
+}
+
+async function iterateAnswer() {
+  const numQuestion = getRandomInt(1, 100);
+  const correctAnswer = numQuestion % 2 === 0 ? 'yes' : 'no';
+  const numAnswer = await askQuestion(`Question: ${numQuestion}`);
+  const right = numAnswer === correctAnswer;
+  if (!right) {
+    printIterateAnswerWrong(numAnswer, correctAnswer);
+  }
+  return right;
+}
+
+async function playGameNumber() {
+  printInstructions();
   const numTries = 3;
   let numCorrectAnswers = 0;
   for (let i = 1; i <= numTries; i += 1) {
-    const numQuestion = getRandomInt(1, 100);
-    const correctAnswer = numQuestion % 2 === 0 ? 'yes' : 'no';
-    const numAnswer = await askQuestion(`Question: ${numQuestion}`);
-    if (numAnswer === correctAnswer) {
-      console.log('Correct!');
+    const correct = await iterateAnswer();
+    if (correct) {
+      printIterateAnswerRight();
       numCorrectAnswers += 1;
     } else {
-      console.log(`'${numAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      break;
+      return false;
     }
   }
-  if (numCorrectAnswers === numTries) {
-    console.log(`Congratulations, ${name}!`);
-  }
+  return numCorrectAnswers === numTries;
+}
+
+const game = async () => {
+  printWelcome();
+  const name = await getName();
+  printGreeting(name);
+  const won = await playGameNumber();
+  printGameResults(won, name);
 };
+
 export default game;
